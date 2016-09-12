b85df4e291174bc7fd24bf0fba377475ecefa837
Michael Newton <figgynewt@ymail.com>
2016-09-12 03:53:20 +0000

    all parts passed the unit tests

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,12 +1,15 @@
-# When done, submit this entire file to the autograder.
+=begin
+Michael Newton
+09/11/2016
+hw-ruby-intro
+Software Engineering
+=end
 
 # Part 1
 
 def sum arr
   total = 0
-  arr.each do |x|
-    total = total + x
-  end
+  arr.each {|x| total += x}
   return total
 end
 
@@ -18,26 +21,61 @@ def max_2_sum arr
 end
 
 def sum_to_n? (arr, n)
-  arr.permutation(2).any?{|n1,n2| n1 + n2 == n}
+  arr.permutation(2).any?{|pair| sum(pair) == n}
 end
 
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " << name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s =~ /\A(?=[^aeiou])(?=[a-z])/i
+    return true
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  dec = s.to_i
+  return false if s.length == 0
+  return true if ((s =~ /^[01]/)  && (dec % 4 == 0))
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  
+  def initialize(isbn, price)
+    if price <= 0
+      raise ArgumentError, "Price Too Small"
+    elsif isbn.length <=0
+      raise ArgumentError, "ISBN invalid"
+    end
+    @isbn = isbn
+    @price = price
+  end
+  
+  def isbn
+    @isbn
+  end
+  
+  def price
+    @price
+  end
+  
+  def isbn=(new_isbn)
+    @isbn = new_isbn
+  end
+  
+  def price=(new_price)
+    @price = new_price
+  end
+  
+  def price_as_string 
+    currencyForm = "$%.2f" % @price
+    return currencyForm
+  end
+  
 end
